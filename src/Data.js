61bfd58b3f08e665960242abe3316e2626c16e19
Ethan Vachon <Ethan.Vachon@jacksons.com>
2022-02-25 10:20:06 -0700

    working with just maj/min in major keys

--- src/Data.js
+++ src/Data.js
@@ -1,22 +1,60 @@
 export let chords = [
-    "i-VI-III-VII",
-    "I-iv-I-V",
-    "I-V-I-V",
+    [1,6,3,7],
+    [1,4,1,5],
+    [1,5,1,5],
+    [2,4,5,1],
+    [3,6,5,1]
 ]
 
 export let keys = [
-    "C",
-    "Db",
-    "D",
-    "Eb",
-    "E",
-    "F",
-    "Gb",
-    "G",
-    "Ab",
-    "A",
-    "Bb",
-    "B",
+    {
+        root: "C",
+        scale: ["C","D","E","F","G","A","B","C"],
+    },
+    {
+        root: "Db",
+        scale: ["Db","Eb","F","Gb","Ab","Bb","C","Db"],
+    },
+    {
+        root: "D",
+        scale: ["D","E","F#","G","A","B","C#","D"],
+    },
+    {
+        root: "Eb",
+        scale: ["Eb","F","G","Ab","Bb","C","D","Eb"],
+    },
+    {
+        root: "E",
+        scale: ["E","F#","G#","A","B","C#","D#","E"],
+    },
+    {
+        root: "F",
+        scale: ["F","G","A","Bb","C","D","E","F"],
+    },
+    {
+        root: "Gb",
+        scale: ["Gb","Ab","Bb","B","Db","Eb","F","Gb"],
+    },
+    {
+        root: "G",
+        scale: ["G","A","B","C","D","E","F#","G"],
+    },
+    {
+        root: "Ab",
+        scale: ["Ab","Bb","C","Db","Eb","F","G","Ab"],
+    },
+    {
+        root: "A",
+        scale: ["A","B","C#","D","E","F#","G#","A"],
+    },
+    {
+        root: "Bb",
+        scale: ["Bb","C","D","Eb","F","G","A","Bb"],
+    },
+    {
+        root: "B",
+        scale: ["B","C#","D#","E","F#","G#","A#","B"],
+    },
 ]
 
 export let modes = [
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <template>
   <button @click="randomize()">Randomize</button><br>
-  {{ state.randomProgression }}<br>
-  {{ state.randomKey }}
-  {{ state.randomMode }}
+  <div v-if="state.ready">
+    <p>In the key of {{ state.randomKey.root }} {{ state.randomMode }}</p>
+    <div style="display:flex;justify-content:center;">
+      <p style="margin:5px" v-for="(chord,i) in state.randomProgression" :key="(chord + i)">{{ chord }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,20 +17,50 @@ export default {
   name: "Main",
   setup() {
     const state = reactive({
-      test: "testing",
+      ready: false,
       keys: keys,
       chords: chords,
       modes: modes,
-      randomProgression: null,
+      unprocessedProgression: null,
+      randomProgression: [],
       randomKey: null,
       randomMode: null,
     })
     return {
       state,
       randomize: function () {
+        this.reset()
+
         this.state.randomKey = this.state.keys[Math.floor(Math.random() * state.keys.length)]
-        this.state.randomProgression = this.state.chords[Math.floor(Math.random() * state.chords.length)]
+
+        this.state.unprocessedProgression = this.state.chords[Math.floor(Math.random() * state.chords.length)]
+
+        for(let i=0;i<this.state.unprocessedProgression.length;i++) {
+          this.determineMinMaj(this.state.unprocessedProgression[i], i)
+        }
+
         this.state.randomMode = this.state.modes[Math.floor(Math.random() * state.modes.length)]
+
+        this.state.ready = true
+      },
+      determineMinMaj: function (key) {
+        let keyInScale = this.state.randomKey.scale[(key-1)]
+        if(keyInScale.includes("#")) {
+          keyInScale = this.state.randomKey.scale[key][0] + "b"
+        }
+        let progressionKey = this.state.keys.find(k => k.root == keyInScale || k.root == keyInScale)
+        if(this.state.randomKey.scale.includes(progressionKey.scale[2])) {
+          this.state.randomProgression.push(keyInScale + " Major")
+        } else {
+          this.state.randomProgression.push(keyInScale + " Minor")
+        }
+        return
+      },
+      reset: function () {
+        this.state.unprocessedProgression = null
+        this.state.randomProgression = []
+        this.state.randomKey = null
+        this.state.randomMode = null
       },
     }
   }
